d7786a44f1ea16ddf45c4602a3bf2b69070c3e33
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2023-02-12 02:06:12 +0530

    chamges in config production

--- config/config.js
+++ config/config.js
@@ -23,6 +23,10 @@ module.exports ={
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "ssl": true,
+    "dialectOptions": {
+      "sslmode": "verify-full"
+    }
   }
 }
