ed9986dab37647ea586e0d5fca9eb3f062ff3d90
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2023-02-13 01:23:25 +0530

    changes in config production

--- config/config.js
+++ config/config.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 module.exports ={
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.PDB_USER,
+    "host": process.env.PDB_HOST,
+    "database": process.env.PDB_NAME,
+    "password": process.env.PDB_PASS,
+    "port": process.env.PDB_PORT,
+    "ssl": { rejectUnauthorized: false },
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.PDB_USER,
+    "host": process.env.PDB_HOST,
+    "database": process.env.PDB_NAME,
+    "password": process.env.PDB_PASS,
+    "port": process.env.PDB_PORT,
+    "ssl": { rejectUnauthorized: false },
     "dialect": "postgres"
   },
   "production": {
