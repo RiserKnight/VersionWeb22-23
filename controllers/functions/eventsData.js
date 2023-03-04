6a2a77a492df395237625011315dfb041ff0a8db
RiserKnight <riser.aayush@gmail.com>
2023-03-04 22:06:55 +0530

    Event data changed for app

--- controllers/functions/eventsData.js
+++ controllers/functions/eventsData.js
@@ -13,6 +13,7 @@ exports.eventsdata=()=>{
             StartTime: "09:00 AM",
             EndTime: "11:00 AM",
             title: "Coding Cauldron",
+            venue: "Third I/MCA Lab",
             desc: "Coding cauldron is a 2 hour long coding challenge in which participants have to solve 3-4 problems based on data structure and algorithms using the programming language of their choice. The participants have to take part individually. Team participation is not allowed. The platform for the event will be HackerEarth(liable to change). "
         }, {
             id: "2",
@@ -25,20 +26,22 @@ exports.eventsdata=()=>{
             EndDate: "18-03-2023",
             StartTime: "11:30 AM",
             EndTime: "01:00 PM",
-            title: "Eliminator",
+            venue: "Third I/MCA Lab",
+            title: "Eliminator Round 2",
             desc: "This event will be played in a team of two. There will be two rounds in this event. In both the rounds, questions based on logical reasoning will be asked. In round one, participants will get points for every correct answer. After round one, Top 30(liable to change) teams will be selected for round 2. "
         }, {
             eventID: "103",
             id: "3",
             image: "https://drive.google.com/uc?export=view&id=1i0a55hHqZg3z69y10bRqJmOKQq7Uxscd",
             day: "1",
-            time1: "03:30 PM - 06:30 PM",
+            time1: "02:30 PM - 06:30 PM",
             StartDate: "18-03-2023",
             EndDate: "18-03-2023",
-            StartTime: "03:30 PM",
+            StartTime: "02:30 PM",
             EndTime: "06:30 PM",
-            time: "3:30 PM",
+            time: "02:30 PM",
             title: "Hacker Hive",
+            venue: "A3",
             desc: "This is a hackathon that will be organized as part of the Version. This will be a 7 days long hackathon. The problem statements for the hackathon will be released online a week before Version. Interested teams can register and develop the solution for any one problem. Problems will be from three domains – Web Development, App Development, Machine Learning. Participating teams will develop the solution and then present that solution offline in Version. "
         }, {
             eventID: "108",
@@ -52,6 +55,7 @@ exports.eventsdata=()=>{
             EndTime: "08:00 PM",
             time: "6:00 PM",
             title: "Just Two Minutes",
+            venue: "Barn Hall/Department",
             desc: "This event is the part of the fun segment. This will be an open stage event. In this event, any candidate can come to the stage and show whatever talent they have such as singing, dancing, stand-up comedy, poetry etc."
         }, {
             eventID: "104",
@@ -64,6 +68,7 @@ exports.eventsdata=()=>{
             EndTime: "01:00 PM",
             time: "10:00 AM",
             title: "Arcanum",
+            venue: "Third I/MCA Lab",
             time1: "10:00 AM - 01:00 PM",
             desc: "Arcanum is an event that is based on SQL queries. The event will be played in a team of two. Teams will be given with a relational schema that they can refer to during the event. The game is based on a story. To reach the final phase of the story, teams have to gather 20 clues by writing SQL queries. Teams must write SQL queries that display only one cell as result. Each clue is either an id of a person or an interview transcript of a person. The team that finishes the story first, will be declared as the winner."
         }, {
@@ -77,6 +82,7 @@ exports.eventsdata=()=>{
             EndTime: "03:30 PM",
             time1: "02:30 PM - 03:30 PM",
             time: "2:30 PM",
+            venue: "Third I/MCA Lab",
             title: "Crack & Swap",
             desc: "This is a 1 hour long team coding challenge. In this challenge, participants have to form a team of two people. Each team will be given two problems to solve. Both the member of the team will start working on different problems. After 30 minutes, both of them will switch the problems and continue from where their teammate left. The platform for the event will be HackerEarth (liable to change)."
         }, {
@@ -91,16 +97,18 @@ exports.eventsdata=()=>{
             time: "4:00 PM",
             time1: "04:00 PM - 06:00 PM",
             title: "Xscape Quest",
+            venue: "N/A",
             desc: "This will be a Treasure hunt. This event will be 3 hrs long. This will be a team event. Each team will have to solve some puzzles to gather clues. With the help of those clues, they will move to the next stage. Finally, the team, that gets to final answer first, will be declared as the winner. The event will be hosted in offline mode and teams may have to explore a few parts of NIT Trichy to complete the quest."
         }, {
             eventID: "107",
             id: "8",
             StartDate: "18-03-2023",
             EndDate: "19-03-2023",
-            StartTime: "04:00 PM",
-            EndTime: "06:00 PM",
+            StartTime: "10:00 AM",
+            EndTime: "05:00 PM",
             day: "3",
-            time1: "04:00 PM - 06:00 PM",
+            venue: "N/A",
+            time1: "10:00 AM - 05:00 PM",
             image: "https://drive.google.com/uc?export=view&id=1swE9mKSCOTQXz31faM3gR9Pm47mAipPq",
             title: "GameFiesta",
             desc: "This will be a two hour long LAN gaming event. The game will be played on solo mode. The game played during the event will be developed by the members of the Event Execution Committee. The best performer of the game will be declared as the winner."
@@ -116,20 +124,23 @@ exports.eventsdata=()=>{
             day: "1",
             image: "https://drive.google.com/uc?export=view&id=1ZoJwzxn0wmueGdf4da2w8xxGxhYYuJDK",
             title: "Manage the Damage",
+            venue: "N/A",
             desc: "It will be a 1 hour long debugging event. The participants will be given some erroneous code. They have to resolve all the errors and remove all the bugs that are present in the code. The code will be in one of the three programming languages – C++,Java or Python.This will be a solo event. Team participation is not allowed.This will be a solo event. Team participation is not allowed."
         }, {
             eventID: "110",
             id: "10",
-            StartDate: "19-03-2023",
+            StartDate: "18-03-2023",
             EndDate: "19-03-2023",
-            StartTime: "06:00 PM",
-            EndTime: "08:00 PM",
+            StartTime: "Entire Day",
+            EndTime: "N/A",
             time: "6:00 PM",
             day: "3",
             time1: "04:00 PM - 06:00 PM",
             image: "https://drive.google.com/uc?export=view&id=1BiBGYVV_lzPnRTZTUZ-HPQ0Iap4ZBzUf",
             title: "Shot vs Shot",
+            venue: "N/A",
             desc: "In this events, the participants have to shoot a video of less than 2 minutes showing NIT Trichy. They have to explore NIT Trichy and shoot as many good things as they can. They have to show their editing and videography skills. This will be a solo event."
+    
         }
     ]
     
